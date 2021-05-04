559f78cfd771ab0c2806323309d070cd7310e9b9
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-04 23:42:30 +0100

    created component for folder

---
+++ src/components/layout/AppFooter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AppFooter.css';
+
+const AppFooter = () => {
+	return (
+		// prettier-ignore
+		<footer>
+			<p>
+				Made by <a href='https://github.com/GuiLucas' target='_blank' rel='noreferrer'>
+					Guilherme Lucas
+				</a>
+				.
+			</p>
+		</footer>
+	);
+};
+
+export default AppFooter;
