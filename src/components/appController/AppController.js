4b3700d6d0916071efec7e65fa6f0f91ffd50454
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-06 20:41:03 +0100

    added comments and minor refactoring

--- src/components/appController/AppController.js
+++ src/components/appController/AppController.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
+
+//	Hooks
 import useDebounce from '../hooks/UseDebounce';
 
+//	Components
 import SearchInput from '../search/SearchInput';
-import MovieItem from '../movieList/MovieItem';
 import MovieList from '../movieList/MovieList';
-import NominationList from '../nominations/NominationList';
-import NominationItem from '../nominations/NominationItem';
+import NominationList from '../nominationList/NominationList';
 import Modal from '../ui/Modal';
+import Card from '../ui/Card';
 
+//	Styling
 import './AppController.css';
 
 const AppController = () => {
@@ -15,19 +18,19 @@ const AppController = () => {
 	const [searchQuery, setSearchQuery] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
 	const [nominations, setNominations] = useState([]);
-	const [nominationFinal, setNominationFinal] = useState(false);
+	const [nominationComplete, setNominationComplete] = useState(false);
 
 	const debouncedSearchQuery = useDebounce(searchQuery, 500);
 
 	useEffect(() => {
 		setIsSearching(true);
-		//Cancel if there's no input
+
 		if (!searchQuery) {
 			return;
 		}
 
 		if (debouncedSearchQuery) {
-			fetchData(debouncedSearchQuery).then((data) => {
+			fetchMovies(debouncedSearchQuery).then((data) => {
 				setSearchResults(data);
 				setIsSearching(false);
 			});
@@ -36,7 +39,6 @@ const AppController = () => {
 		}
 	}, [debouncedSearchQuery, searchQuery]);
 
-	//Clean up localStorage logic
 	useEffect(() => {
 		const savedNominations = JSON.parse(localStorage.getItem('nominations'));
 
@@ -45,18 +47,22 @@ const AppController = () => {
 		}
 	}, []);
 
-	// When it reaches 5 nominations show banner
 	useEffect(() => {
 		localStorage.setItem('nominations', JSON.stringify(nominations));
-		//TODO: Implement modal instead of alert and don't show more results?
+
 		if (nominations.length >= 5) {
-			setNominationFinal(true);
+			setNominationComplete(true);
 		} else {
-			setNominationFinal(false);
+			setNominationComplete(false);
 		}
 	}, [nominations]);
 
-	const fetchData = (query) => {
+	/**
+	 * Function to fetch movies from the OMDB API
+	 * @param {String} query 			Query from search input
+	 * @returns {Array of Objects} 		Movie results from the query
+	 */
+	const fetchMovies = (query) => {
 		const apiKey = 'bcba87b7';
 		const url = `https://www.omdbapi.com/?s=${query}&type=movie&apikey=${apiKey}`;
 		return fetch(url)
@@ -68,16 +74,25 @@ const AppController = () => {
 			});
 	};
 
+	/**
+	 * Function to assign the value of the input to the searchQuery state
+	 * @param {String} value 	Value from input
+	 */
 	const handleSearch = (value) => {
-		// If user clears searchQuery after the search results have already been populated
+		// If user clears input after search results have already been presented
 		if (value === '') {
 			setSearchResults([]);
 		}
 		setSearchQuery(value);
 	};
 
+	/**
+	 * Function to add nominations
+	 * @param {Object} movie 	Movie nominated
+	 * @returns empty if nominations is complete
+	 */
 	const handleNomination = (movie) => {
-		if (nominations.length >= 5) {
+		if (nominationComplete) {
 			return;
 		} else if (!nominations.some((result) => result.imdbID === movie.imdbID)) {
 			setNominations([...nominations, movie]);
@@ -86,6 +101,10 @@ const AppController = () => {
 		}
 	};
 
+	/**
+	 * Function to remove a movie from nominations
+	 * @param {Object} movie	Movie to remove
+	 */
 	const removeNomination = (movie) => {
 		if (nominations.some((result) => result.imdbID === movie.imdbID)) {
 			setNominations(
@@ -97,23 +116,24 @@ const AppController = () => {
 	let searchList = null;
 
 	if (searchResults && searchResults.length > 0) {
-		searchList =
-			searchResults &&
-			searchResults.map((movie) => {
-				const isNominated = nominations.some(
-					(result) => result.imdbID === movie.imdbID
-				);
+		searchList = searchResults.map((movie) => {
+			const isNominated = nominations.some(
+				(result) => result.imdbID === movie.imdbID
+			);
 
-				return (
-					<MovieItem
-						key={movie.imdbID}
-						title={movie.Title}
-						year={movie.Year}
-						disabled={isNominated}
-						handleNomination={() => handleNomination(movie)}
-					/>
-				);
-			});
+			return (
+				<Card
+					key={movie.imdbID}
+					title={movie.Title}
+					year={movie.Year}
+					disabled={isNominated || nominationComplete}
+					onClick={() => handleNomination(movie)}
+					listType={'movie-item'}
+					buttonType={'nominate'}
+					buttonContent={isNominated ? 'Nominated' : 'Nominate'}
+				/>
+			);
+		});
 	}
 
 	let nominationList = null;
@@ -121,11 +141,14 @@ const AppController = () => {
 	if (nominations) {
 		nominationList = nominations.map((movie) => {
 			return (
-				<NominationItem
+				<Card
 					key={movie.imdbID}
 					title={movie.Title}
 					year={movie.Year}
-					removeNomination={() => removeNomination(movie)}
+					onClick={() => removeNomination(movie)}
+					listType={'nomination-item'}
+					buttonType={'remove'}
+					buttonContent={'Remove'}
 				/>
 			);
 		});
@@ -138,13 +161,14 @@ const AppController = () => {
 			{!searchList && !isSearching ? (
 				<h2 className='no-results'>There's no results for {searchQuery}</h2>
 			) : null}
+
 			{searchList && (
 				<MovieList searchQuery={searchQuery} searchList={searchList} />
 			)}
 
 			{nominationList && <NominationList nominationList={nominationList} />}
 
-			{nominationFinal && (
+			{nominationComplete && (
 				<Modal info={'You have nominated all your movies!'} />
 			)}
 		</main>
@@ -152,8 +176,3 @@ const AppController = () => {
 };
 
 export default AppController;
-
-//TODO add message when there's no search results
-// kinda done, see if there's other way
-// Style H2
-//TODO Remove buttons when nomination = 5
