6a71678f8870b6eb3c5af7544717abcad39f8642
Guilucas <guilhermehenriqueslucas@gmail.com>
2021-05-06 19:34:00 +0100

    strech sections across page height

--- src/components/layout/AppWrapper.js
+++ src/components/layout/AppWrapper.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import AppController from '../appController/AppController';
 import AppHeader from './AppHeader';
+import AppFooter from './AppFooter';
+import './AppWrapper.css';
 
 const AppWrapper = () => {
 	return (
-		<>
+		<div className='wrapper'>
 			<AppHeader />
 			<AppController />
-		</>
+			<AppFooter />
+		</div>
 	);
 };
 //TODO Add Footer
